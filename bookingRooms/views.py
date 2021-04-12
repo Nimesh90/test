b0cbf4a3d2df13f3838dd02ac63465d885f2e5e2
aashishk9091@gmail.com <aashishk9091@gmail.com>
2021-04-12 10:29:09 +0545

    htttp response

--- bookingRooms/views.py
+++ bookingRooms/views.py
@@ -1,6 +1,6 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return render()
+    return render(request, HttpResponse(heelow adasdas))
